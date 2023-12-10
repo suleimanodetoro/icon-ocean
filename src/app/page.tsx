b2076414b56b3e214becbef14ab0db9c0ceca89d
Suleiman Odetoro <odetoro75@gmail.com>
2023-12-10 18:19:41 +0000

    Change Shad CN theme

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,11 +1,12 @@
+import MaxWidthWrapper from '@/components/MaxWidthWrapper'
 import Image from 'next/image'
 
 export default function Home() {
   return (
-    <main>
-      <h1>
-        Hello World
-      </h1>
-    </main>
+    <MaxWidthWrapper>
+      <div className='py-20 mx-auto text-center flex flex-col '>
+
+      </div>
+    </MaxWidthWrapper>
   )
 }
