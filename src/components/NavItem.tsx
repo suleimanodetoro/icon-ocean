e0cb888bf47e6fb0799ca6a0c40f08d1f69a2908
Suleiman Odetoro <odetoro75@gmail.com>
2023-12-13 22:43:26 +0000

    Implement dropdown on nav item click

--- src/components/NavItem.tsx
+++ src/components/NavItem.tsx
@@ -4,9 +4,11 @@ import { PRODUCT_CATEGROIES } from "@/config";
 import { Button } from "./ui/button";
 import { ChevronDown } from "lucide-react";
 import { cn } from "@/lib/utils";
+import Image from "next/image";
+import Link from "next/link";
 
 // the type "Category" should be a single element of our array
-type Category = (typeof PRODUCT_CATEGROIES)[number];
+type Category = typeof PRODUCT_CATEGROIES[number];
 
 interface NavItemProps {
   category: Category;
@@ -32,6 +34,57 @@ const NavItem = ({ isAnyOpen, category, isOpen, handleOpen }: NavItemProps) => {
           />
         </Button>
       </div>
+      { isOpen ? (
+        <div
+        className={cn(
+          "absolute inset-x-0 top-full text-sm text-muted-foreground",
+          {
+            "animate-in fade-in-10 slide-in-from-top-5": !isAnyOpen,
+          }
+        )}
+      >
+        {/* self closing div to add shadow. Aria hidden so it does not show to screen readers */}
+        <div
+          aread-hidden
+          className="absolute inset-0 top-1/2 bg-white shadow"
+        />
+
+        <div className="relative mx-auto max-w-7xl px-8">
+          <div className="grid grid-cols-4 gap-x-8 gap-y-10 py-16">
+            <div className="col-span-4 col-start-1 grid grid-cols-3 gap-x-8">
+              {/* category content to show when nav item is opened */}
+              {category.featured.map((item) => (
+                <div
+                  key={item.name}
+                  className="group relative text-base sm:text-sm"
+                >
+                  <div className="relative aspect-video overflow-hidden rounded-lg bg-gray-100 group-hover:opacity-75 ">
+                    <Image
+                      src={item.imageSrc}
+                      alt="Product category image"
+                      fill
+                      className="object-cover object-center"
+                    />
+                  </div>
+                  {/* link to click on for specific product item */}
+                  <Link
+                    href={item.href}
+                    className="mt-6 block font-medium text-gray-900 "
+                  >
+                    {item.name}
+                    <p aria-hidden className="mt-1">
+                      Shop now
+                    </p>
+                  </Link>
+                </div>
+              ))}
+            </div>
+          </div>
+        </div>
+      </div>
+      ) : null
+        
+      }
     </div>
   );
 };
